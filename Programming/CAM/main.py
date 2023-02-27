453a10b893927a2b38b8911440dfbb36ba2f97c2
AbdulRahman Badran <ambadran717@hotmail.com>
2023-02-27 21:13:57 +0200

    MinorChanges

--- Programming/CAM/main.py
+++ Programming/CAM/main.py
@@ -8,12 +8,12 @@ from holes_gcode_generator import create_generate_holes_gcode
 
 if __name__ == '__main__':
 
-    file_path = 'default.gbr'
+    file_path = 'gerber_files/default.gbr'
     user_x_offset = 5
     user_y_offset = 5
     feedrate = 600
     spindle_speed = 230
-    file_name = 'default.gcode'
+    file_name = 'gcode_files/default.gcode'
 
     recenter_gerber_file(file_path, user_x_offset, user_y_offset)
 
