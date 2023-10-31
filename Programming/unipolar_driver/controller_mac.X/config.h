e438694d0b26d098d8da9a7334843f25bd1daf0f
AbdulRahman Badran <ambadran717@hotmail.com>
2023-10-31 10:33:51 +0200

    Fixed diode directions in unipolar_driver circuit

--- Programming/unipolar_driver/controller_mac.X/config.h
+++ Programming/unipolar_driver/controller_mac.X/config.h
@@ -6,7 +6,7 @@
 // User Definned Configs
 //#define DEBUG_MODE
 //#define CONSTANT_STEPS
-//#define ENABLE_SOFT_UART  // comment to enable usage of GPIO5 as debug pin
+#define ENABLE_SOFT_UART  // comment to enable usage of GPIO5 as debug pin
 
 #if(defined(ENABLE_SOFT_UART) && defined(DEBUG_MODE))
 #error  // Can't enable software uart and enable debug pin in the same time
